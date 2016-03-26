d67a76dd0fc160fdebeadce5294ae212a636eb29
Alexander Lenz <alenz@dev-platypus.org>
2016-03-26 16:58:27 +0100

    gui: implement plenty of features.
    Change-Id: I3302c7d9bd147faad42e681c938f29c5a6aedb8c
    Reviewed-on: https://review.gerrithub.io/270508
    Reviewed-by: Alexander Lenz <alenz@dev-platypus.org>
    Tested-by: Alexander Lenz <alenz@dev-platypus.org>

--- marche/guing/util.py
+++ marche/guing/util.py
@@ -1,6 +1,6 @@
 #  -*- coding: utf-8 -*-
 # *****************************************************************************
-# MLZ server control daemon
+# Marche - A server control daemon
 # Copyright (c) 2015 by the authors, see LICENSE
 #
 # This program is free software; you can redistribute it and/or modify it under
